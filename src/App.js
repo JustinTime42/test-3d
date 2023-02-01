bddb88eaf0a6b58dbd80c5376a2b298e12998575
JustinTime42 <justincase18@msn.com>
2023-02-01 12:24:57 -0900

    cleaned up

--- src/App.js
+++ src/App.js
@@ -11,49 +11,50 @@ export default function Home() {
 
   if (isReadingNfts) {
       return <div>Loading...</div>
-  } else {
-    return (
-      <div>        
-        <Carousel style={{ width: '500px', height: '300px',  marginRight: 'auto', marginLeft: 'auto' }}>
-          {nfts?.map((nft, i) => (
-              <Carousel.Item key={i}>
-                <ThirdwebNftMedia 
-                  metadata={nft.metadata} 
-                />
-                <Carousel.Caption>{nft.metadata.description}</Carousel.Caption>
-              </Carousel.Item>
-          ))}
-        </Carousel>
-        <Button style={{ margin:'2em' }} variant="primary" onClick={() => setShowModal(true)}>Show NFT in Modal</Button>
-        <Modal show={showModal} onHide={() => setShowModal(false)}>
-          <Modal.Header closeButton>
-              <Modal.Title>NFT in Modal</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ThirdwebNftMedia metadata={nfts[1].metadata} />
-          </Modal.Body>
-        </Modal>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>                            
-          {nfts.map((nft, i) => (
-            <Card>
-              <Card.Title>NFT in Bootstrap card</Card.Title>
-              <Card.Body>
-              <ThirdwebNftMedia
-                key={i}
-                metadata={nft.metadata}
-                width= "80px"
-                 height= "30px"
-                style={{ marginRight: 'auto', marginLeft: 'auto' }}
-              />
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
-      </div>      
-    )
   }
+  return (
+    <div>        
+      <Carousel style={{ width: '500px', height: '300px',  marginRight: 'auto', marginLeft: 'auto' }}>
+        {nfts?.map((nft, i) => (
+          <Carousel.Item key={i}>
+            <ThirdwebNftMedia 
+              metadata={nft.metadata} 
+            />
+            <Carousel.Caption>{nft.metadata.description}</Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <Button style={{ margin:'2em' }} variant="primary" onClick={() => setShowModal(true)}>Show NFT in Modal</Button>
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+            <Modal.Title>NFT in Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ThirdwebNftMedia metadata={nfts[1].metadata} />
+        </Modal.Body>
+      </Modal>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>                            
+        {nfts.map((nft, i) => (
+          <Card>
+            <Card.Title>NFT in Bootstrap card</Card.Title>
+            <Card.Body>
+            <ThirdwebNftMedia
+              key={i}
+              metadata={nft.metadata}
+              /*  Try alternating between:
+                  style={{ width: "200px", height: "300px"}
+                  and:
+                  width= "200px"
+                  height= "100px" 
+                  Note, the style will over ride the width/height props
+                  But using one or the other works fine
+              */
+              style={{ width: "200px", height: "300px", marginRight: 'auto', marginLeft: 'auto' }}
+            />
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </div>      
+  )
 }
-
-
-
-
